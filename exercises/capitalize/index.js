1b9941f24fb946de6723ee3a115069f01fd90c1c
David Echelberger <eberger727@gmail.com>
2024-05-05 19:27:47 -0400

    [master] 8 - capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,41 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const titleCase = (str) => {
+    if (str.length === 0) {
+      return str;
+    }
+
+    return `${str.charAt(0).toUpperCase()}${
+      str.length > 1 ? str.slice(1) : ""
+    }`;
+  };
+
+  return str
+    .split(" ")
+    .map((w) => {
+      return titleCase(w);
+    })
+    .join(" ");
+}
+
+// function capitalize(str) {
+//     const charWords = [];
+//     str.split("").map((w, idx) => {
+//       if (idx === 0) {
+//         charWords.push(w.toUpperCase());
+//         return;
+//       }
+
+//       if (str.charAt(idx - 1) === " ") {
+//         charWords.push(w.toUpperCase());
+//       } else {
+//         charWords.push(w);
+//       }
+//     });
+
+//     return charWords.join("");
+//   }
 
 module.exports = capitalize;
