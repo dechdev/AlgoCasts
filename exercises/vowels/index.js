bc1164d9352097fe0a5786064d165f99f3691cf0
David Echelberger <eberger727@gmail.com>
2024-05-05 22:48:53 -0400

    [master] 11 - vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let numVowels = 0;
+
+  str.split("").map((c) => {
+    if (vowels.includes(c.toLowerCase())) {
+      numVowels++;
+    }
+  });
+
+  return numVowels;
+}
 
 module.exports = vowels;
