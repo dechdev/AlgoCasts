4eef06c9159fe478df5ef339c371e802e1b31ad7
David Echelberger <eberger727@gmail.com>
2024-05-07 08:11:09 -0400

    [master] 23 - levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const countArr = [0];
+  const q = [root, "s"];
+
+  while (q.length > 1) {
+    const currNode = q.shift();
+    if (currNode !== "s") {
+      countArr[countArr.length - 1] = countArr[countArr.length - 1] + 1;
+      q.push(...currNode.children);
+    } else {
+      countArr.push(0);
+      q.push("s");
+    }
+  }
+
+  return countArr;
+}
 
 module.exports = levelWidth;
